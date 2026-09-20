266bf7b63da14ff024f2022f2c462fe663f6556f
Amritanshu Praveen <82750605+Neo-XD@users.noreply.github.com>
2026-09-20 17:56:18 +0530

    fix(web): add svelte.config.js, simplify vite.config, fix base path for GitHub Pages

--- vite.config.ts
+++ vite.config.ts
@@ -1,5 +1,4 @@
 import tailwindcss from '@tailwindcss/vite';
-import adapter from '@sveltejs/adapter-static';
 import { sveltekit } from '@sveltejs/kit/vite';
 import { defineConfig } from 'vite';
 
@@ -34,15 +33,6 @@ export default defineConfig({
 			}
 		},
 		tailwindcss(),
-		sveltekit({
-			adapter: adapter({ fallback: 'index.html' }),
-			paths: {
-				base: process.env.GITHUB_PAGES === 'true' ? '/nocturne-web' : ''
-			},
-			compilerOptions: {
-				// Force runes mode for the project, except for libraries. Can be removed in svelte 6.
-				runes: ({ filename }) => filename.split(/[/\\]/).includes('node_modules') ? undefined : true
-			}
-		})
+		sveltekit()
 	]
 });
